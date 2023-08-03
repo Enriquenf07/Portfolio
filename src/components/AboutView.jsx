566e8504a3afc5e91db0aebef7fd4a4ea7740b1d
Enrique <enriqueferreira956@gmail.com>
2023-08-03 14:33:19 -0300

    v2

--- src/components/AboutView.jsx
+++ src/components/AboutView.jsx
@@ -52,7 +52,49 @@ export default function AboutView(){
                     </div>
             </section>
             </div>
-            
         )
     }
+    if (isActiveAbout) {
+        return (
+            <section className="border border-black rounded-lg shadow shadow-black w-[30rem] ">
+                    <div className="h-10 bg-red-300 border border-b-black rounded-t-lg flex justify-end gap-[23rem] pr-5">
+                        <p className="text-base flex items-center">About</p>
+                        <button className="text-xl" onClick={() => setIsActiveAbout(false)}>X</button>
+                    </div>
+                    <div className="p-10 flex flex-col gap-8">
+                        <div>
+                            <h1 className="text-4xl ">Enrique Ferreira</h1>
+                            <h2 className="text-xl text-purple-300">Web Developer</h2>
+                        </div>
+                        <p className="">I have solid experience working with UI, Apis and Databases. Interested in functional programming, algorithms and software architecture.</p>
+                        <button className="w-[10rem] h-[2rem] bg-lime-200 rounded-lg border-4 border-t-2 border-l-2 border-black">Projects</button>
+                        <div className="flex gap-2">
+                            <AiFillGithub size={25} />
+                            <AiFillLinkedin size={25} />
+                        </div>
+                    </div> 
+                </section>
+        )
+    }
+    if (isActiveSkills) {
+        return (
+            <section className="border border-black rounded-lg shadow shadow-black w-[19rem] h-[20rem]">
+                    <div className="h-10 bg-red-300 border border-b-black rounded-t-lg flex justify-end gap-[9.8rem] pr-5">
+                            <p className="text-base flex items-center">Experience</p>
+                            <button className="text-xl" onClick={() => setIsActiveSkills(false)}>X</button>
+                        </div>
+                        <div className="flex flex-wrap gap-10 w-[20rem] p-10">
+                            <BiLogoJavascript size={45} color={'#F0DB4F'}/>
+                            <BiLogoReact size={45} color={'#61dbfb'} />
+                            <BiLogoPython size={45} />
+                            <BiLogoDjango size={45} color={'#092e20'}/>
+                            <BiLogoGoLang size={45} color={'#61dbfb'} />
+                            <BiLogoPostgresql size={45} color={'#0064a5'}/>
+                            <BiLogoMongodb size={45} color={'#4DB33D'}/>
+                            <DiRedis size={45} color={'#D82C20'}/>
+                        </div>
+                </section>
+        )
+    }
+    
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 export default function NavBar(props){
     
     return (
-    <nav className="h-[2rem] w-[4rem] flex items-center">
+    <nav className="h-[2rem] w-[4rem] flex items-center gap-1">
         {props.children}
     </nav>
     )
